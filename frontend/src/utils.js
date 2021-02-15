068ecae064bc20db9b162536adc3bf189e61b622
srajan2627 <46938701+srajan2627@users.noreply.github.com>
2021-02-15 13:36:41 -0600

    share code on github

---
+++ frontend/src/utils.js
@@ -0,0 +1,39 @@
+export const prices = [
+    {
+      name: 'Any',
+      min: 0,
+      max: 0,
+    },
+    {
+      name: `$1 to $10`,
+      min: 1,
+      max: 10,
+    },
+    {
+      name: `$10 to $50`,
+      min: 10,
+      max: 50,
+    },
+
+  ];
+  export const ratings = [
+    {
+      name: '4stars & up',
+      rating: 4,
+    },
+  
+    {
+      name: '3stars & up',
+      rating: 3,
+    },
+  
+    {
+      name: '2stars & up',
+      rating: 2,
+    },
+  
+    {
+      name: '1stars & up',
+      rating: 1,
+    },
+  ];
